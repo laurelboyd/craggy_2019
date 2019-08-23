412a3ca0cb44296bcbe080ae800e6eb2222f8a6e
Jim Tyhurst <jim@tyhurst.com>
2019-08-23 06:06:44 -0700

    Enhanced the package documentation.

---
+++ R/craggy2019.R
@@ -0,0 +1,9 @@
+#' craggy2019: Housing and eviction data for King County, Washington, United States.
+#'
+#' This package provides data, as well as some utilities for accessing that
+#' data. See the `vignettes` for sample code for exploring and analyzing
+#' the data.
+#'
+#' @docType package
+#' @name craggy2019
+NULL
